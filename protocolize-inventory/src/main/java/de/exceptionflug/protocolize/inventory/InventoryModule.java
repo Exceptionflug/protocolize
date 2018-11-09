Exceptionflug/protocolize

commit fc2d5004958c9755186285e09e7197260bc46328

Mojang can't count. Implementing manual id count for GameStateChange reasons

protocolize-inventory/src/main/java/de/exceptionflug/protocolize/inventory/adapter/ClickWindowAdapter.java
@@ -30,51 +30,50 @@ public class ClickWindowAdapter extends PacketAdapter<ClickWindow> {
     public void receive(final PacketReceiveEvent<ClickWindow> event) {
         final ClickWindow clickWindow = event.getPacket();
         final Inventory inventory = InventoryModule.getInventory(event.getPlayer().getUniqueId(), clickWindow.getWindowId());
-        if(inventory == null) {
-            ProxyServer.getInstance().getLogger().warning("[Protocolize] "+event.getPlayer().getName()+" has no open inventory with id "+clickWindow.getWindowId());
+        if (inventory == null) {
+            ProxyServer.getInstance().getLogger().warning("[Protocolize] " + event.getPlayer().getName() + " has no open inventory with id " + clickWindow.getWindowId());
             return;
         }
 
         short slot = clickWindow.getSlot();
-        if(inventory.getType() == InventoryType.BREWING_STAND && ReflectionUtil.getProtocolVersion(event.getPlayer()) == MINECRAFT_1_8) {
+        if (inventory.getType() == InventoryType.BREWING_STAND && ReflectionUtil.getProtocolVersion(event.getPlayer()) == MINECRAFT_1_8) {
             // Insert missing slot 4 for 1.8 clients into slot calculation
-            if(slot >= 4)
-                slot ++;
+            if (slot >= 4)
+                slot++;
         }
 
         final InventoryClickEvent clickEvent = new InventoryClickEvent(event.getPlayer(), inventory, slot, clickWindow.getWindowId(), clickWindow.getClickType(), clickWindow.getActionNumber());
         ProxyServer.getInstance().getPluginManager().callEvent(clickEvent);
 
-        if(clickEvent.isCancelled() || inventory.isHomebrew()) {
+        if (clickEvent.isCancelled() || inventory.isHomebrew()) {
             event.setCancelled(true);
-            if(InventoryModule.getInventory(event.getPlayer().getUniqueId(), clickWindow.getWindowId()) != null) {
-                if(inventory.getType() != InventoryType.PLAYER)
-                    InventoryModule.sendInventory(event.getPlayer(), inventory);
-                else
-                    InventoryManager.getInventory(event.getPlayer().getUniqueId()).update();
-            }
-            event.getPlayer().unsafe().sendPacket(new SetSlot((byte)-1, (short)-1, new ItemStack(ItemType.NO_DATA)));
+            event.getPlayer().sendMessage(clickEvent.getClickedItem() == null ? "EMPTY" : clickEvent.getClickedItem().getType().name());
+            if (inventory.getType() != InventoryType.PLAYER)
+                InventoryModule.sendInventory(event.getPlayer(), inventory);
+            else
+                InventoryManager.getInventory(event.getPlayer().getUniqueId()).update();
+            event.getPlayer().unsafe().sendPacket(new SetSlot((byte) -1, (short) -1, new ItemStack(ItemType.NO_DATA)));
 //            event.getPlayer().unsafe().sendPacket(new ConfirmTransaction((byte)clickWindow.getWindowId(), (short) clickWindow.getActionNumber(), false));
             return;
         }
-        if(clickWindow.getActionNumber() != clickEvent.getActionNumber()) {
+        if (clickWindow.getActionNumber() != clickEvent.getActionNumber()) {
             clickWindow.setActionNumber(clickEvent.getActionNumber());
             event.markForRewrite();
         }
-        if(clickWindow.getWindowId() != clickEvent.getWindowId()) {
+        if (clickWindow.getWindowId() != clickEvent.getWindowId()) {
             clickWindow.setWindowId(clickEvent.getWindowId());
             event.markForRewrite();
         }
-        if(clickWindow.getClickType() != clickEvent.getClickType()) {
+        if (clickWindow.getClickType() != clickEvent.getClickType()) {
             clickWindow.setClickType(clickEvent.getClickType());
             event.markForRewrite();
         }
-        if(slot != clickEvent.getSlot()) {
+        if (slot != clickEvent.getSlot()) {
             clickWindow.setSlot((short) clickEvent.getSlot());
             event.markForRewrite();
         }
 
-        if(InventoryModule.isSpigotInventoryTracking()) {
+        if (InventoryModule.isSpigotInventoryTracking()) {
             handleClick(clickEvent, event);
         }
     }
@@ -84,22 +83,22 @@ public class ClickWindowAdapter extends PacketAdapter<ClickWindow> {
         final InventoryAction action = new InventoryAction();
         action.setPreviousCursor(playerInventory.getOnCursor());
 
-        if(clickEvent.getClickType() == ClickType.LEFT_CLICK) {
-            if(playerInventory.getOnCursor() == null) {
-                if(clickEvent.getClickedItem() != null) {
+        if (clickEvent.getClickType() == ClickType.LEFT_CLICK) {
+            if (playerInventory.getOnCursor() == null) {
+                if (clickEvent.getClickedItem() != null) {
                     action.setNewCursor(clickEvent.getClickedItem().deepClone());
                     action.removeItem(clickEvent.getSlot());
                 }
             } else {
-                if(clickEvent.getClickedItem() == null) {
+                if (clickEvent.getClickedItem() == null) {
                     action.setItem(clickEvent.getSlot(), playerInventory.getOnCursor().deepClone());
                     action.setNewCursor(null);
                 } else {
-                    if(clickEvent.getClickedItem().canBeStacked(playerInventory.getOnCursor())) {
-                        if(clickEvent.getClickedItem().getAmount() + playerInventory.getOnCursor().getAmount() > clickEvent.getClickedItem().getType().getMaxStackSize()) {
+                    if (clickEvent.getClickedItem().canBeStacked(playerInventory.getOnCursor())) {
+                        if (clickEvent.getClickedItem().getAmount() + playerInventory.getOnCursor().getAmount() > clickEvent.getClickedItem().getType().getMaxStackSize()) {
                             final int tillFull = clickEvent.getClickedItem().getType().getMaxStackSize() - clickEvent.getClickedItem().getAmount();
                             final ItemStack newCursor = playerInventory.getOnCursor().deepClone();
-                            newCursor.setAmount((byte) (newCursor.getAmount()-tillFull));
+                            newCursor.setAmount((byte) (newCursor.getAmount() - tillFull));
                             action.setNewCursor(newCursor);
                             final ItemStack stack = clickEvent.getClickedItem().deepClone();
                             stack.setAmount((byte) stack.getType().getMaxStackSize());
@@ -116,9 +115,9 @@ public class ClickWindowAdapter extends PacketAdapter<ClickWindow> {
                     }
                 }
             }
-        } else if(clickEvent.getClickType() == ClickType.RIGHT_CLICK) {
-            if(playerInventory.getOnCursor() == null) {
-                if(clickEvent.getClickedItem() != null) {
+        } else if (clickEvent.getClickType() == ClickType.RIGHT_CLICK) {
+            if (playerInventory.getOnCursor() == null) {
+                if (clickEvent.getClickedItem() != null) {
                     final ItemStack inv = clickEvent.getClickedItem().deepClone();
                     final ItemStack cursor = inv.deepClone();
                     final double half = (double) cursor.getAmount() / 2;
@@ -128,14 +127,14 @@ public class ClickWindowAdapter extends PacketAdapter<ClickWindow> {
                     action.setItem(clickEvent.getSlot(), inv);
                 }
             } else {
-                if(clickEvent.getClickedItem() == null) {
+                if (clickEvent.getClickedItem() == null) {
                     final ItemStack newItem = playerInventory.getOnCursor().deepClone();
                     newItem.setAmount((byte) 1);
                     action.setItem(clickEvent.getSlot(), newItem);
 
                     final ItemStack cursor = playerInventory.getOnCursor().deepClone();
-                    cursor.setAmount((byte) (cursor.getAmount() -1));
-                    if(cursor.getAmount() <= 0) {
+                    cursor.setAmount((byte) (cursor.getAmount() - 1));
+                    if (cursor.getAmount() <= 0) {
                         action.setNewCursor(null);
                     } else {
                         action.setNewCursor(cursor);
@@ -143,13 +142,13 @@ public class ClickWindowAdapter extends PacketAdapter<ClickWindow> {
                 } else {
                     final ItemStack cursor = playerInventory.getOnCursor().deepClone();
                     final ItemStack current = clickEvent.getClickedItem().deepClone();
-                    if(current.canBeStacked(cursor)) {
-                        if(current.getAmount()+1 < current.getType().getMaxStackSize()) {
-                            cursor.setAmount((byte) (cursor.getAmount()-1));
-                            current.setAmount((byte) (current.getAmount()+1));
+                    if (current.canBeStacked(cursor)) {
+                        if (current.getAmount() + 1 < current.getType().getMaxStackSize()) {
+                            cursor.setAmount((byte) (cursor.getAmount() - 1));
+                            current.setAmount((byte) (current.getAmount() + 1));
                             action.setItem(clickEvent.getSlot(), current);
                         }
-                        if(cursor.getAmount() <= 0) {
+                        if (cursor.getAmount() <= 0) {
                             action.setNewCursor(null);
                         } else {
                             action.setNewCursor(cursor);
@@ -160,38 +159,38 @@ public class ClickWindowAdapter extends PacketAdapter<ClickWindow> {
                     }
                 }
             }
-        } else if(clickEvent.getClickType() == ClickType.CREATIVE_MIDDLE_CLICK) {
-            if(WorldModule.getGamemode(event.getPlayer().getUniqueId()) != null && WorldModule.getGamemode(event.getPlayer().getUniqueId()) == Gamemode.CREATIVE) {
+        } else if (clickEvent.getClickType() == ClickType.CREATIVE_MIDDLE_CLICK) {
+            if (WorldModule.getGamemode(event.getPlayer().getUniqueId()) != null && WorldModule.getGamemode(event.getPlayer().getUniqueId()) == Gamemode.CREATIVE) {
                 final ItemStack newCursor = clickEvent.getClickedItem().deepClone();
                 newCursor.setAmount((byte) newCursor.getType().getMaxStackSize());
                 action.setNewCursor(newCursor);
             } else {
                 action.setNewCursor(playerInventory.getOnCursor());
             }
-        } else if(clickEvent.getClickType() == ClickType.DROP) {
-            if(clickEvent.getSlot() == -999) {
+        } else if (clickEvent.getClickType() == ClickType.DROP) {
+            if (clickEvent.getSlot() == -999) {
                 action.setNewCursor(null);
             } else {
                 final ItemStack inv = clickEvent.getClickedItem().deepClone();
-                inv.setAmount((byte) (inv.getAmount() -1));
-                if(inv.getAmount() <= 0) {
+                inv.setAmount((byte) (inv.getAmount() - 1));
+                if (inv.getAmount() <= 0) {
                     action.removeItem(clickEvent.getSlot());
                 } else {
                     action.setItem(clickEvent.getSlot(), inv);
                 }
             }
-        } else if(clickEvent.getClickType() == ClickType.DROP_ALL) {
+        } else if (clickEvent.getClickType() == ClickType.DROP_ALL) {
             action.setNewCursor(null);
-        } else if(clickEvent.getClickType() == ClickType.DRAG_START_LEFT || clickEvent.getClickType() == ClickType.DRAG_START_CREATIVE_MIDDLE || clickEvent.getClickType() == ClickType.DRAG_START_RIGHT) {
+        } else if (clickEvent.getClickType() == ClickType.DRAG_START_LEFT || clickEvent.getClickType() == ClickType.DRAG_START_CREATIVE_MIDDLE || clickEvent.getClickType() == ClickType.DRAG_START_RIGHT) {
             action.beginDragging();
             action.setNewCursor(playerInventory.getOnCursor());
-        } else if(clickEvent.getClickType() == ClickType.DRAG_STOP_LEFT || clickEvent.getClickType() == ClickType.DRAG_STOP_CREATIVE_MIDDLE || clickEvent.getClickType() == ClickType.DRAG_STOP_RIGHT) {
+        } else if (clickEvent.getClickType() == ClickType.DRAG_STOP_LEFT || clickEvent.getClickType() == ClickType.DRAG_STOP_CREATIVE_MIDDLE || clickEvent.getClickType() == ClickType.DRAG_STOP_RIGHT) {
             action.endDragging(clickEvent.getClickType());
             action.setNewCursor(playerInventory.getOnCursor());
-        } else if(clickEvent.getClickType() == ClickType.DRAG_ADD_SLOT_LEFT || clickEvent.getClickType() == ClickType.DRAG_ADD_SLOT_CREATIVE_MIDDLE || clickEvent.getClickType() == ClickType.DRAG_ADD_SLOT_RIGHT) {
+        } else if (clickEvent.getClickType() == ClickType.DRAG_ADD_SLOT_LEFT || clickEvent.getClickType() == ClickType.DRAG_ADD_SLOT_CREATIVE_MIDDLE || clickEvent.getClickType() == ClickType.DRAG_ADD_SLOT_RIGHT) {
             action.setDragSlot(clickEvent.getSlot());
             action.setNewCursor(playerInventory.getOnCursor());
-        } else if(clickEvent.getClickType() == ClickType.SHIFT_LEFT_CLICK) {
+        } else if (clickEvent.getClickType() == ClickType.SHIFT_LEFT_CLICK) {
 
         }
         InventoryModule.registerInventoryAction(event.getPlayer().getUniqueId(), clickEvent.getWindowId(), clickEvent.getActionNumber(), action);

protocolize-api/pom.xml
@@ -25,7 +25,5 @@
     <groupId>de.exceptionflug</groupId>
     <artifactId>protocolize-api</artifactId>
     <version>1.0-SNAPSHOT</version>
-
-
     
 </project>

protocolize-inventory/src/main/java/de/exceptionflug/protocolize/inventory/packet/ConfirmTransaction.java
@@ -40,7 +40,7 @@ public class ConfirmTransaction extends AbstractPacket {
         MAPPING_SERVERBOUND.put(MINECRAFT_1_10, 0x05);
         MAPPING_SERVERBOUND.put(MINECRAFT_1_11, 0x05);
         MAPPING_SERVERBOUND.put(MINECRAFT_1_11_1, 0x05);
-        MAPPING_SERVERBOUND.put(MINECRAFT_1_12, 0x05);
+        MAPPING_SERVERBOUND.put(MINECRAFT_1_12, 0x06);
         MAPPING_SERVERBOUND.put(MINECRAFT_1_12_1, 0x05);
         MAPPING_SERVERBOUND.put(MINECRAFT_1_12_2, 0x05);
         MAPPING_SERVERBOUND.put(MINECRAFT_1_13, 0x06);

protocolize-items/src/main/java/de/exceptionflug/protocolize/items/adapter/HeldItemChangeAdapter.java
@@ -15,6 +15,8 @@ public class HeldItemChangeAdapter extends PacketAdapter<HeldItemChange> {
 
     @Override
     public void receive(final PacketReceiveEvent<HeldItemChange> event) {
+        if(event.getServerInfo() == null)
+            return;
         final PlayerInventory inventory = InventoryManager.getInventory(event.getPlayer().getUniqueId(), event.getServerInfo().getName());
         inventory.setHeldItem(event.getPacket().getNewSlot());
     }

protocolize-plugin/src/main/java/de/exceptionflug/protocolize/netty/ProtocolizeDecoderChannelHandler.java
@@ -112,7 +112,6 @@ public class ProtocolizeDecoderChannelHandler extends MessageToMessageDecoder<Pa
         if (cause.getClass().equals(CancelSendSignal.INSTANCE.getClass()))
             throw ((Error) cause);
         if(cause instanceof ClosedChannelException) {
-            ProxyServer.getInstance().getLogger().warning("[Protocolize] Connection interrupted: "+connection.toString()+" @ protocol "+protocolVersion+" for "+stream.name()+" (channel closed)");
             return;
         } else if(cause instanceof NativeIoException) {
             return; // Suppress this annoying shit...

protocolize-world/src/main/java/de/exceptionflug/protocolize/world/packet/ChangeGameState.java
@@ -61,13 +61,13 @@ public class ChangeGameState extends AbstractPacket {
 
     @Override
     public void read(final ByteBuf buf, final Direction direction, final int protocolVersion) {
-        reason = Reason.values()[buf.readUnsignedByte()];
+        reason = Reason.getById(buf.readUnsignedByte());
         value = buf.readFloat();
     }
 
     @Override
     public void write(final ByteBuf buf, final Direction direction, final int protocolVersion) {
-        buf.writeByte(((byte)reason.ordinal()) & 0xFF);
+        buf.writeByte(((byte)reason.getId()) & 0xFF);
         buf.writeFloat(value);
     }
 
@@ -95,7 +95,25 @@ public class ChangeGameState extends AbstractPacket {
 
     public enum Reason {
 
-        INVALID_BED, END_RAINING, BEGIN_RAINING, CHANGE_GAMEMODE, EXIT_END, DEMO_MESSAGE, ARROW_HITTING_PLAYER, FADE_VALUE, FADE_TIME, ELDER_GUARDIAN_APPEARANCE
+        INVALID_BED(0), END_RAINING(1), BEGIN_RAINING(2), CHANGE_GAMEMODE(3), EXIT_END(4), DEMO_MESSAGE(5), ARROW_HITTING_PLAYER(6), FADE_VALUE(7), FADE_TIME(8), ELDER_GUARDIAN_APPEARANCE(10);
+
+        private final int id;
+
+        Reason(final int id) {
+            this.id = id;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public static Reason getById(final int id) {
+            for(final Reason r : values()) {
+                if(r.getId() == id)
+                    return r;
+            }
+            return null;
+        }
 
     }
 

commit d34b3c5bb992ec0ceef5aecf9e621ea63b27ccd3

Fix bug caused in commit fc2d5004958c9755186285e09e7197260bc46328

protocolize-inventory/src/main/java/de/exceptionflug/protocolize/inventory/InventoryModule.java
@@ -31,7 +31,8 @@ public final class InventoryModule {
 
     private static boolean spigotInventoryTracking = false;
 
-    private InventoryModule() {}
+    private InventoryModule() {
+    }
 
     public static void initModule() {
         // TO_CLIENT
@@ -66,7 +67,7 @@ public final class InventoryModule {
 
     public static void registerInventory(final UUID playerId, final int windowId, final Inventory inventory) {
         Preconditions.checkNotNull(playerId, "The playerId cannot be null!");
-        if(inventory == null) {
+        if (inventory == null) {
             WINDOW_MAP.computeIfAbsent(playerId, (id) -> Maps.newHashMap()).remove(windowId);
             ACTION_MAP.computeIfAbsent(playerId, (id) -> Maps.newHashMap()).remove(windowId);
             return;
@@ -79,7 +80,7 @@ public final class InventoryModule {
     }
 
     public static Inventory getInventory(final UUID playerId, final int windowId) {
-        if(windowId == 0) {
+        if (windowId == 0) {
             final PlayerInventory inventory = InventoryManager.getCombinedSendInventory(playerId, ProxyServer.getInstance().getPlayer(playerId).getServer().getInfo().getName());
             final Inventory out = new Inventory(InventoryType.PLAYER, 46);
             out.setItems(inventory.getItemsIndexed());
@@ -95,10 +96,10 @@ public final class InventoryModule {
 
     private static int generateWindowId(final UUID playerId) {
         int out = WINDOW_ID_COUNTER_MAP.getOrDefault(playerId, 101);
-        if(out >= 200) {
+        if (out >= 200) {
             out = 101;
         }
-        WINDOW_ID_COUNTER_MAP.put(playerId, out+1);
+        WINDOW_ID_COUNTER_MAP.put(playerId, out + 1);
         return out;
     }
 
@@ -121,22 +122,22 @@ public final class InventoryModule {
     public static void sendInventory(final ProxiedPlayer p, Inventory inventory) {
         Preconditions.checkNotNull(p, "The player cannot be null!");
         Preconditions.checkNotNull(inventory, "The inventory cannot be null!");
-        if(inventory.getType() == InventoryType.PLAYER)
+        if (inventory.getType() == InventoryType.PLAYER)
             throw new IllegalStateException("Inventory type PLAYER is only for internal usage");
 
         boolean alreadyOpen = false;
         int windowId = -1;
         final Map<Integer, Inventory> playerMap = WINDOW_MAP.computeIfAbsent(p.getUniqueId(), (id) -> Maps.newHashMap());
-        for(final Integer id : playerMap.keySet()) {
+        for (final Integer id : playerMap.keySet()) {
             final Inventory val = playerMap.get(id);
-            if(val == inventory) {
+            if (val == inventory) {
                 alreadyOpen = true;
                 break;
             }
         }
 
         // Close all inventories if not opened
-        if(!alreadyOpen) {
+        if (!alreadyOpen) {
             final Map<Integer, Inventory> toDel = Maps.newHashMap();
             final Inventory finalInventory = inventory;
             getInventories(p.getUniqueId()).forEach((id, it) -> {
@@ -146,15 +147,15 @@ public final class InventoryModule {
             toDel.forEach((id, it) -> registerInventory(p.getUniqueId(), id, null));
         }
 
-        if(playerMap.containsValue(inventory)) {
-            for(final Integer id : playerMap.keySet()) {
+        if (playerMap.containsValue(inventory)) {
+            for (final Integer id : playerMap.keySet()) {
                 final Inventory val = playerMap.get(id);
-                if(val == inventory) {
+                if (val == inventory) {
                     windowId = id;
                     break;
                 }
             }
-            if(windowId == -1) {
+            if (windowId == -1) {
                 windowId = generateWindowId(p.getUniqueId());
                 registerInventory(p.getUniqueId(), windowId, inventory);
             }
@@ -165,26 +166,26 @@ public final class InventoryModule {
 
         final InventoryOpenEvent event = new InventoryOpenEvent(p, inventory, windowId);
         ProxyServer.getInstance().getPluginManager().callEvent(event);
-        if(event.isCancelled())
+        if (event.isCancelled())
             return;
         windowId = event.getWindowId();
         inventory = event.getInventory();
 
-        if(inventory.getType() == InventoryType.CRAFTING_TABLE)
+        if (inventory.getType() == InventoryType.CRAFTING_TABLE)
             inventory.setSize(0);
 
         if(!alreadyOpen)
             p.unsafe().sendPacket(new OpenWindow(windowId, inventory.getSize(), inventory.getType(), inventory.getTitle()));
         final List<ItemStack> items = Lists.newArrayList(inventory.getItemsIndexed());
 
-        if(inventory.getType() == InventoryType.BREWING_STAND && ReflectionUtil.getProtocolVersion(p) == MINECRAFT_1_8)
+        if (inventory.getType() == InventoryType.BREWING_STAND && ReflectionUtil.getProtocolVersion(p) == MINECRAFT_1_8)
             items.remove(4);
 
         if(ItemsModule.isSpigotInventoryTracking()) {
             final PlayerInventory playerInventory = InventoryManager.getInventory(p.getUniqueId());
             items.addAll(playerInventory.getItemsIndexedContainer());
         }
-        p.unsafe().sendPacket(new WindowItems((short)windowId, items));
+        p.unsafe().sendPacket(new WindowItems((short) windowId, items));
     }
 
     public static void setSpigotInventoryTracking(final boolean spigotInventoryTracking) {

protocolize-inventory/src/main/java/de/exceptionflug/protocolize/inventory/adapter/ClickWindowAdapter.java
@@ -47,11 +47,13 @@ public class ClickWindowAdapter extends PacketAdapter<ClickWindow> {
 
         if (clickEvent.isCancelled() || inventory.isHomebrew()) {
             event.setCancelled(true);
-            if (inventory.getType() != InventoryType.PLAYER)
-                InventoryModule.sendInventory(event.getPlayer(), inventory);
-            else
-                InventoryManager.getInventory(event.getPlayer().getUniqueId()).update();
-            event.getPlayer().unsafe().sendPacket(new SetSlot((byte) -1, (short) -1, new ItemStack(ItemType.NO_DATA)));
+            if(InventoryModule.getInventory(event.getPlayer().getUniqueId(), clickWindow.getWindowId()) != null) {
+                if (inventory.getType() != InventoryType.PLAYER)
+                    InventoryModule.sendInventory(event.getPlayer(), inventory);
+                else
+                    InventoryManager.getInventory(event.getPlayer().getUniqueId()).update();
+                event.getPlayer().unsafe().sendPacket(new SetSlot((byte) -1, (short) -1, new ItemStack(ItemType.NO_DATA)));
+            }
 //            event.getPlayer().unsafe().sendPacket(new ConfirmTransaction((byte)clickWindow.getWindowId(), (short) clickWindow.getActionNumber(), false));
             return;
         }