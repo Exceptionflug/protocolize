0d510605707eb0828d5ccd29c487a31532b66fc2
Nico Britze <n.britze@exceptionflug.de>
2019-12-07 20:31:36 +0100

    Deprecate setCancelSend and isCancelSend

--- protocolize-api/src/main/java/de/exceptionflug/protocolize/api/protocol/AbstractPacket.java
+++ protocolize-api/src/main/java/de/exceptionflug/protocolize/api/protocol/AbstractPacket.java
@@ -17,10 +17,18 @@ public abstract class AbstractPacket extends DefinedPacket {
             throw CancelSendSignal.INSTANCE;
     }
 
+    @Deprecated
     public boolean isCancelSend() {
         return cancelSend;
     }
 
+    /**
+     * If set to true, the packet handler will throw an instance of {@link CancelSendSignal}. Since this is deprecated,
+     * use the setCancelled(boolean cancelled) method of the {@link de.exceptionflug.protocolize.api.event.PacketReceiveEvent}
+     * or {@link de.exceptionflug.protocolize.api.event.PacketSendEvent} instead.
+     * @param cancelSend
+     */
+    @Deprecated
     public void setCancelSend(final boolean cancelSend) {
         this.cancelSend = cancelSend;
     }
