3347d3f4828b8084a632a636ec9d75d11188bcdb
LogGits <landogowner@gmail.com>
2020-03-12 12:37:05 +1000

    Permission for ProxyInv command.

--- protocolize-plugin/src/main/java/de/exceptionflug/protocolize/command/ProxyInvCommand.java
+++ protocolize-plugin/src/main/java/de/exceptionflug/protocolize/command/ProxyInvCommand.java
@@ -5,16 +5,21 @@ import de.exceptionflug.protocolize.items.PlayerInventory;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
+import net.md_5.bungee.api.chat.TextComponent;
 
 public class ProxyInvCommand extends Command {
 
     public ProxyInvCommand() {
-        super("proxyinv");
+        super("proxyinv", "protocolize.command.proxyinv");
     }
 
     @Override
     public void execute(final CommandSender commandSender, final String[] strings) {
         if(commandSender instanceof ProxiedPlayer) {
+            if(!commandSender.hasPermission(this.getPermission())){
+                commandSender.sendMessage(new TextComponent("§cYou don't have permission to use this command"));
+                return;
+            }
             final ProxiedPlayer p = (ProxiedPlayer) commandSender;
             final PlayerInventory inventory = InventoryManager.getCombinedSendInventory(p.getUniqueId(), p.getServer().getInfo().getName());
             inventory.update();
