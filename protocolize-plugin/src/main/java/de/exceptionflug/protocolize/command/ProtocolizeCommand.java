0753a479b0b2d4fde91b243b171854c64039a5e6
LogGits <landogowner@gmail.com>
2020-03-12 12:41:45 +1000

    Add permission to protocolize command.

--- protocolize-plugin/src/main/java/de/exceptionflug/protocolize/command/ProtocolizeCommand.java
+++ protocolize-plugin/src/main/java/de/exceptionflug/protocolize/command/ProtocolizeCommand.java
@@ -2,6 +2,7 @@ package de.exceptionflug.protocolize.command;
 
 import de.exceptionflug.protocolize.ProtocolizePlugin;
 import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.plugin.Command;
 
 public class ProtocolizeCommand extends Command {
@@ -9,12 +10,16 @@ public class ProtocolizeCommand extends Command {
     private final ProtocolizePlugin plugin;
 
     public ProtocolizeCommand(final ProtocolizePlugin plugin) {
-        super("protocolize");
+        super("protocolize", "protocolize.command.protocolize");
         this.plugin = plugin;
     }
 
     @Override
     public void execute(final CommandSender commandSender, final String[] args) {
+        if(!commandSender.hasPermission(this.getPermission())){
+            commandSender.sendMessage(new TextComponent("§cYou don't have permission to use this command"));
+            return;
+        }
         if(args.length == 0) {
             commandSender.sendMessage("§aProtocolize version "+plugin.getDescription().getVersion()+" by "+plugin.getDescription().getAuthor());
             if(commandSender.hasPermission("protocolize.toggle")) {
